fe3241f28b4c90cc00d6ea163f5be1dbcf9d69f4
Rahul Gopinath <rahul@gopinath.org>
2017-09-11 17:19:57 +0200

    update

--- src/grammar.py
+++ src/grammar.py
@@ -1,4 +1,12 @@
-import tracer
+import sys
+
+class Tracer(object):
+    def __init__(self, method):
+        self.method = method
+    def __enter__(self):
+        sys.settrace(self.method)
+    def __exit__(self, type, value, traceback):
+        sys.settrace(None)
 
 class Grammar(object):
     def __init__(self, method, inputs):
