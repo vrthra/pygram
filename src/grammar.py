c2e8f3b3ba7cfa7affb931f116ab7e5e6cae2073
Rahul Gopinath <rahul@gopinath.org>
2017-09-11 19:11:37 +0200

    one step back

--- src/grammar.py
+++ src/grammar.py
@@ -26,25 +26,20 @@ class Multidict(collections.defaultdict):
     def merge(self, g2):
         for k,v in g2.items(): self[k] = self[k] | v
 
-class RSet(set):
-    def replace(self, key, replacement):
-        self.remove(key)
-        self.add(replacement)
-
 class Grammar(object):
-    def __init__(self): self.grammar = Multidict(RSet)
+    def __init__(self): self.grammar = Multidict(set)
 
     def __str__(self): return self.grammar_to_string(self.grammar)
 
     def grammar_to_string(self, grammar):
         return "\n".join(["%s ::= %s" % (key, " | ".join(grammar[key])) for key in grammar.keys()])
 
-    def nt(self, var): return "$" + var.upper()
+    def nonterminal(self, var): return "$" + var.upper()
 
     # Obtain a grammar for a specific input
-    def get_grammar(self, my_input, local_values):
+    def get_grammar(self, my_input, my_values):
         # Here's our initial grammar
-        grammar = {"$START": RSet([my_input])}
+        grammar = {"$START": set([my_input])}
 
         # Now for each (VAR, VALUE) found:
         # 1. We search for occurrences of VALUE in the grammar
@@ -52,23 +47,26 @@ class Grammar(object):
         # 3. We add a new rule $VAR -> VALUE to the grammar
         while True:
             new_rules = []
-            for localvar, localval in local_values.items():
-                for key, inputvalues in grammar.items():
-                    rules = [(self.nt(localvar), localval, ivalue) for ivalue in inputvalues if localval in ivalue]
+            for var, value in my_values.items():
+                for key, val in grammar.items():
+                    myvalues = val
+                    values_to_replace = [s for s in val if value in s]
+                    for repl in values_to_replace:
+                        myvalues.remove(repl)
 
-                    for (nt_myvar, my_val, r) in rules:
-                        inputvalues.replace(r, r.replace(my_val, nt_myvar))
+                        alt_key = self.nonterminal(var)
+                        myvalues.add(repl.replace(value, alt_key))
 
-                    new_rules += [(localvar, nt, lval) for (nt, lval, _) in rules]
+                        new_rules += [(var, alt_key, value)]
 
             if len(new_rules) == 0: break # Nothing to expand anymore
 
-            for (lvar, ntkey, lvalue) in new_rules:
+            for (var, alt_key, value) in new_rules:
                 # Add new rule to grammar
-                grammar[ntkey] = RSet([lvalue])
+                grammar[alt_key] = set([value])
 
                 # Do not expand this again
-                del local_values[lvar]
+                del my_values[var]
 
         return grammar
 
