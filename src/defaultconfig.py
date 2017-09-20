879e00b682a1bc7e303663475a47b83cc57a4f8d
Rahul Gopinath <rahul@gopinath.org>
2017-09-20 13:22:32 +0200

    update

--- src/defaultconfig.py
+++ src/defaultconfig.py
@@ -4,4 +4,10 @@ check_globals = False
 non_trivial = lambda var,val: len(var) >= 1 and len(val) >= 2
 in_scope = lambda fn: True
 decorate = False
+
+# in functions that take a self parameter, it is often the case
+# that many members of the self object remain unused. Unless we
+# scan and remove unused parameters, on grammar merge, the unused
+# self objects combines with the same attribute in other functions
+# where they are decomposed.
 strip_unused_params = False
