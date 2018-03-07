668a04357bc373d6a559b5cecf9c622fe7a33185
Rahul Gopinath <rahul@gopinath.org>
2018-03-07 20:28:44 +0100

    compactify

--- miner.py
+++ miner.py
@@ -1,30 +1,28 @@
 #!/usr/bin/env python3
 # Mine a grammar from dynamic behavior
 
-import pudb
 import tstr
+import pudb
 brk = pudb.set_trace
 
 class V:
     def __init__(self, v):
-        self.v = v
         self._taint = v._taint
         self._index_map = {range(0, len(self._taint)): v}
 
     def __lt__(self, o): return str(self).__lt__(str(o))
 
     def value(self):
-        return ''.join([self._index_map[k] for k in
-            sorted(self._index_map.keys(), key=lambda a: a.start)])
+        return ''.join([self._index_map[k]
+            for k in sorted(self._index_map.keys(), key=lambda a: a.start)])
 
     def __repr__(self): return 'V:%s' % self.value()
 
     def cur_taint(self):
-        t = [-1] * len(self.v)
+        t = [-1] * len(self.taint)
         for k in sorted(self._index_map.keys(), key=lambda a: a.start):
             v = self._index_map[k]
-            if type(v) is tstr.tstr:
-                t[k.start:k.stop] = v._taint
+            if type(v) is tstr.tstr: t[k.start:k.stop] = v._taint
         return t
 
     def _tinclude(self, o):
@@ -34,7 +32,7 @@ class V:
         return None
 
     def _in_range(self, o):
-        return self._taint[0] <= o._taint[0] and self.taint[-1] >= o._taint[-1]
+        return self.taint[0] <= o._taint[0] and self.taint[-1] >= o._taint[-1]
 
     @property
     def taint(self): return self._taint
@@ -46,7 +44,7 @@ class V:
             # When we paste the taints of new word in, (not replace)
             # then, does it get us more coverage? (i.e more -1s)
             cur_tsum = sum(i for i in self.cur_taint() if i < 0)
-            t = self._taint[:]
+            t = self.taint[:]
             start_i = t.index(gword._taint[0])
             stop_i = t.index(gword._taint[-1])
             t[start_i:stop_i+1] = [-1] * len(gword)
@@ -73,21 +71,18 @@ class V:
             self._index_map[range(o._taint[0], o._taint[-1]+1)] = repl
             return
 
-        sorig = orig
         # get starting point.
         taintkey, reprange = taintft
         my_str = self._index_map[taintkey]
         del self._index_map[taintkey]
-        # insert the fraction between taintkey and frm
-        # which corresponds to my_str
-        # my_str[0] = taintkey.start
-        x = (reprange.start - taintkey.start)
+        splitA = reprange.start - taintkey.start
+        splitB = splitA + len(orig)
         init_range = range(taintkey.start, reprange.start)
         mid_range = range(reprange.start, reprange.stop)
         end_range = range(reprange.stop, taintkey.stop)
-        if init_range: self._index_map[init_range] = my_str[0:x]
+        if init_range: self._index_map[init_range] = my_str[0:splitA]
         self._index_map[mid_range] = repl
-        if end_range: self._index_map[end_range] = my_str[x + len(sorig):]
+        if end_range: self._index_map[end_range] = my_str[splitB:]
 
 # Convert a variable name into a grammar nonterminal
 def nonterminal(var): return "$" + var.upper()
