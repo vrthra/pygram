b72854d83b2ae25877dfb08181c554bbdfc99a42
Rahul Gopinath <rahul@gopinath.org>
2018-03-07 22:30:38 +0100

    compactify

--- miner.py
+++ miner.py
@@ -6,19 +6,17 @@ import grammar as g
 class V:
     def __init__(self, v):
         self._taint = v._taint
-        self._rindex = {range(0, len(self._taint)): v}
+        self._rindex = {tainted_range(self._taint): v}
 
+    def ranges(self): return sorted(self._rindex, key=lambda a: a.start)
     def __lt__(self, o): return str(self).__lt__(str(o))
     def __repr__(self): return 'V:%s' % self.value()
-
-    def value(self):
-        return ''.join([self._rindex[k]
-            for k in sorted(self._rindex, key=lambda a: a.start)])
+    def value(self): return ''.join(self._rindex[k] for k in self.ranges())
 
 
     def cur_taint(self):
         t = [-1] * len(self.taint)
-        for k in sorted(self._rindex, key=lambda a: a.start):
+        for k in self.ranges():
             v = self._rindex[k]
             if hasattr(v, '_taint'): t[k.start:k.stop] = v._taint
         return t
@@ -26,21 +24,21 @@ class V:
     def _tinclude(self, o):
         return next((k for k in self._rindex if set(o._taint) <= set(k)), None)
 
-    def _in_range(self, o):
+    def _encloses(self, o):
         return self.taint[0] <= o._taint[0] and self.taint[-1] >= o._taint[-1]
 
     @property
     def taint(self): return self._taint
 
     def include(self, word):
-        if not self._in_range(word): return None
+        if not self._encloses(word): return None
         # When we paste the taints of new word in, (not replace)
         # then, does it get us more coverage? (i.e more -1s)
         cur_tsum = sum(i for i in self.cur_taint() if i < 0)
-        t = self.taint[:]
-        start_i,stop_i = t.index(word._taint[0]),t.index(word._taint[-1])
-        t[start_i:stop_i+1] = [-1] * len(word)
-        new_sum = sum(i for i in t if i < 0)
+        ts = self.taint[:]
+        start_i, stop_i = ts.index(word._taint[0]), ts.index(word._taint[-1])
+        ts[start_i:stop_i+1] = [-1] * len(word)
+        new_sum = sum(i for i in ts if i < 0)
         if new_sum <= cur_tsum: return new_sum - cur_tsum
         return None
 
@@ -48,37 +46,37 @@ class V:
         # we need to handle these
         # s = peek(1)
         # if s == 't': read_str('true')
-        # or 
+        # or
         # i = read_num(), point = read('.'), d = read_num()
         # number = i + point + d
-        taintkey = self._tinclude(o)
-        if not taintkey:
+        trange = tainted_range(o._taint)
+        keytaint = self._tinclude(o)
+        if not keytaint:
             # the complete taint range is not contained, but we are still
             # inclued in the original. It means that an inbetween variable has
             # obscured our inclusion.
             to_rem = []
-            for k in sorted(self._rindex, key=lambda a: a.start):
-                if o._taint[0] <= k.start <= o._taint[-1]:
-                    assert o._taint[0] <= k.stop <= o._taint[-1]+1
+            for k in self.ranges():
+                if k.start in trange:
+                    assert k.stop - 1 in trange
                     to_rem.append(k)
             for k in to_rem: del self._rindex[k]
-            self._rindex[range(o._taint[0], o._taint[-1]+1)] = repl
+            self._rindex[trange] = repl
             return
 
         # get starting point.
-        reprange = range(o._taint[0], o._taint[-1] + 1)
-        my_str = self._rindex[taintkey]
-        del self._rindex[taintkey]
-        splitA = reprange.start - taintkey.start
-        splitB = splitA + len(o)
-        init_range = range(taintkey.start, reprange.start)
-        mid_range = range(reprange.start, reprange.stop)
-        end_range = range(reprange.stop, taintkey.stop)
+        my_str = self._rindex[keytaint]
+        del self._rindex[keytaint]
+        splitA = trange.start - keytaint.start
+        init_range = range(keytaint.start, trange.start)
+        mid_range = range(trange.start, trange.stop)
+        end_range = range(trange.stop, keytaint.stop)
         if init_range: self._rindex[init_range] = my_str[0:splitA]
         self._rindex[mid_range] = repl
-        if end_range: self._rindex[end_range] = my_str[splitB:]
+        if end_range: self._rindex[end_range] = my_str[splitA + len(o):]
 
 def nonterminal(var): return "$" + var.upper()
+def tainted_range(tarr): return range(tarr[0], tarr[-1]+1)
 
 # Obtain a grammar for a specific input
 def get_grammar(assignments):
@@ -88,7 +86,6 @@ def get_grammar(assignments):
         nt_var = nonterminal(var)
         append = False if my_grammar else True
         for _, repl_alternatives in my_grammar.items():
-            # if value taint is a proper subset of repl taint
             res = [repl for repl in repl_alternatives if repl.include(value)]
             for repl in res: repl.replace(value, nt_var)
             append = True
